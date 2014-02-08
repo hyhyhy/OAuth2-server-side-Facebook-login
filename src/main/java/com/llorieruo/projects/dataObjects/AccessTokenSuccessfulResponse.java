e09a16efbbfc1779996d2344a36a61c3d46c3d37
Luís Loureiro <llorieruo@sapo.pt>
2014-02-08 18:34:41 +0000

    Data objects used to represent responses obtained from requests made to the OAuth2 authorization server or OAuth2 resource server. Implementing data objects specific to the Facebook API.

---
+++ src/main/java/com/llorieruo/projects/dataObjects/AccessTokenSuccessfulResponse.java
@@ -0,0 +1,52 @@
+package com.llorieruo.projects.dataObjects;
+
+/**
+ * Represents a successful response when issuing the authorization server for an access token.
+ * @see http://tools.ietf.org/html/rfc6749#section-5.1
+ */
+public class AccessTokenSuccessfulResponse extends ErrorResponse
+{
+	private String	access_token, refresh_token, token_type, scope;
+	private long	expires_in;
+	
+	public String getAccess_token()
+	{
+		return access_token;
+	}
+	public void setAccess_token(String access_token)
+	{
+		this.access_token = access_token;
+	}
+	public String getRefresh_token()
+	{
+		return refresh_token;
+	}
+	public void setRefresh_token(String refresh_token)
+	{
+		this.refresh_token = refresh_token;
+	}
+	public String getToken_type()
+	{
+		return token_type;
+	}
+	public void setToken_type(String token_type)
+	{
+		this.token_type = token_type;
+	}
+	public String getScope()
+	{
+		return scope;
+	}
+	public void setScope(String scope)
+	{
+		this.scope = scope;
+	}
+	public long getExpires_in()
+	{
+		return expires_in;
+	}
+	public void setExpires_in(long expires_in)
+	{
+		this.expires_in = expires_in;
+	}
+}
---
+++ src/main/java/com/llorieruo/projects/dataObjects/facebook/FacebookInspectedToken.java
@@ -0,0 +1,56 @@
+package com.llorieruo.projects.dataObjects.facebook;
+
+public class FacebookInspectedToken extends FacebookApiErrorResponse
+{
+	private Data	data;
+	
+	public Data getData()
+	{
+		return data;
+	}
+
+	public void setData(Data data)
+	{
+		this.data = data;
+	}
+	
+	public class Data
+	{
+		private String	app_id, user_id;
+		private String[] scopes;
+		private boolean is_valid;
+		
+		public String getApp_id()
+		{
+			return app_id;
+		}
+		public void setApp_id(String app_id)
+		{
+			this.app_id = app_id;
+		}
+		public String getUser_id()
+		{
+			return user_id;
+		}
+		public void setUser_id(String user_id)
+		{
+			this.user_id = user_id;
+		}
+		public String[] getScopes()
+		{
+			return scopes;
+		}
+		public void setScopes(String[] scopes)
+		{
+			this.scopes = scopes;
+		}
+		public boolean isIs_valid()
+		{
+			return is_valid;
+		}
+		public void setIs_valid(boolean is_valid)
+		{
+			this.is_valid = is_valid;
+		}
+	}
+}
